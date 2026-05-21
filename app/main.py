0ebf2cd7e971fd1b63d142402e35ca20edb74bd3
Belly433 <bellydynella.sabushi@final.edu.tr>
2026-05-21 17:29:48 +0300

    Phase 2 backend complete

--- app/main.py
+++ app/main.py
@@ -5,9 +5,16 @@ from app.models.user_model import User
 from app.models.quiz_model import Quiz
 from app.routes import quiz
 from app.routes import anti_cheat
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router, prefix="/auth")
 app.include_router(quiz.router, prefix="/quiz")
 app.include_router(anti_cheat.router, prefix="/anti-cheat")
